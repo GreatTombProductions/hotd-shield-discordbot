899eebd7df7b7795d74fb9a3e050c91051bad2fb
Raymond Heberer <ray.heberer@greattombproductions.com>
2022-03-24 17:18:13 -0700

    Refactor code into functions

--- main.py
+++ main.py
@@ -4,8 +4,14 @@ import pickle
 from datetime import datetime, timedelta
 from keep_alive import keep_alive
 
+ADMIN = "AvatarOfNazarick"
+COMMAND = "!shield"
+
+example_text = 'For example, to register an 8 hour shield: "!shield 8 Luggergue"'
+
 client = discord.Client()
 
+# restore most recent version of shields (in case of server outage)
 try:
     with open ('shields.pkl', 'rb') as fp:
         shields = pickle.load(fp)
@@ -17,6 +23,115 @@ def format_timedelta(td):
     hours, minutes = divmod(minutes, 60)
     return '{:d}h:{:02d}m'.format(hours, minutes)
 
+async def register_new_shield(message, content, shields):
+    global ADMIN, example_text 
+
+    correctly_formatted = True
+    # add a shield
+    user = message.author
+
+    admin_mode = False
+    if user.name == ADMIN:
+        try:
+            float(content[1])
+        except:
+            admin_mode = True
+
+    if admin_mode:
+        username = content[1]
+        shield_length = content[2]
+    else:
+        shield_length = content[1]
+
+    # check that time is formatted correctly
+    try:
+        shield_length = float(shield_length)
+    except:
+        tosend = 'Shield length formatted incorrectly, try again. ' + example_text
+        await message.channel.send(tosend)
+        correctly_formatted = False
+        return
+
+    # check that time is between bounds
+    if shield_length < 0 or shield_length > 72:
+        tosend = 'Shield length must be between 0 and 72 hours. ' + example_text
+        await message.channel.send(tosend)
+        correctly_formatted = False
+        return
+
+    if len(content) > 2 and not admin_mode:
+        description = " ".join(content[2:])
+    elif len(content) > 3 and admin_mode:
+        description = " ".join(content[3:])
+    else:
+        description = ""
+
+    start_time = datetime.now()
+
+    if not admin_mode:
+        if user.nick is None:
+            username = user.name
+        else:
+            username = user.nick
+
+    if correctly_formatted:
+        entry = {"user": username,
+                 "amount": shield_length,
+                 "description" : description,
+                 "start_time": start_time,
+                 "remaining_time": shield_length}
+
+        shields = [shield for shield in shields if shield["user"] != entry["user"]]
+                
+        shields.append(entry)
+    return shields
+
+async def print_current_shields(message, shields):
+    global example_text
+    
+    tosend = "Currently active shields:"
+
+    # remove shields that have expired
+    valid_shields = []
+    for i, shield in enumerate(shields):
+        hours_active = datetime.now() - shield["start_time"]
+        remaining_time = timedelta(hours=shield["amount"]) - hours_active
+
+        if remaining_time > timedelta(0):
+            valid_shields.append(i)
+        
+        remaining_time = format_timedelta(remaining_time)
+        shield["remaining_time"] = remaining_time
+
+    shields = [shields[i] for i in range(len(shields)) if i in valid_shields]
+
+    # if there are no shields
+    if len(shields) == 0:
+        tosend += ' none.\nTo register a shield, say "!shield time description" where time is a number between 0 and 72 representing hours. ' + example_text
+        await message.channel.send(tosend)
+        return
+
+    # print each shield on its own line
+    shields = sorted(shields, key=lambda d: d['remaining_time'], reverse=True)
+
+    for shield in shields:
+        row = f"\n{shield['user']}, {shield['remaining_time']} remaining"
+        if len(shield["description"]) > 0:
+            row = row + f", {shield['description']}"
+
+        tosend = tosend + row
+
+    tosend = tosend + '\n\nRegister a new shield with "!shield time description" where time is a number between 0 and 72 representing hours. ' + example_text
+
+    # save current shields
+    with open("shields.pkl", "wb") as fp:
+        pickle.dump(shields, fp)
+
+    await message.channel.send(tosend)
+    return
+
+
+
 @client.event
 async def on_ready():
     print('We have logged in as {0.user}'.format(client))
@@ -27,103 +142,15 @@ async def on_message(message):
     if message.author == client.user:
         return
 
-    if message.content.startswith('!shield'):
+    if message.content.lower().startswith(COMMAND):
         content = message.content.split(" ")
 
         if len(content) > 1:
-            new_shield = True
-            # add a shield
-            user = message.author
+            # register new shield
+            shields = await register_new_shield(message, content, shields)
 
-            admin_mode = False
-            if user.name == "AvatarOfNazarick":
-                try:
-                    float(content[1])
-                except:
-                    admin_mode = True
-
-            if admin_mode:
-                username = content[1]
-                shield_length = content[2]
-            else:
-                shield_length = content[1]
-
-            try:
-                shield_length = float(shield_length)
-            except:
-                tosend = 'Shield length formatted incorrectly, try again. For example,to register an 8 hour shield: "!shield 8 Luggergue"'
-                await message.channel.send(tosend)
-                new_shield = False
-                return
-
-            if shield_length < 0 or shield_length > 72:
-                tosend = 'Shield length must be between 0 and 72 hours. For example,to register an 8 hour shield: "!shield 8 Luggergue"'
-                await message.channel.send(tosend)
-                new_shield = False
-                return
-
-            if len(content) > 2 and not admin_mode:
-                description = " ".join(content[2:])
-            elif len(content) > 3 and admin_mode:
-                description = " ".join(content[3:])
-            else:
-                description = ""
-
-            start_time = datetime.now()
-
-            if not admin_mode:
-                if user.nick is None:
-                    username = user.name
-                else:
-                    username = user.nick
-
-            if new_shield:
-                entry = {"user": username,
-                         "amount": shield_length,
-                         "description" : description,
-                         "start_time": start_time,
-                         "remaining_time": shield_length}
-    
-                shields = [shield for shield in shields if shield["user"] != entry["user"]]
-                        
-                shields.append(entry)
         # display current shields
-        tosend = "Currently active shields:"
-
-        valid_shields = []
-        for i, shield in enumerate(shields):
-            hours_active = datetime.now() - shield["start_time"]
-            remaining_time = timedelta(hours=shield["amount"]) - hours_active
-
-            if remaining_time > timedelta(0):
-                valid_shields.append(i)
-            
-            remaining_time = format_timedelta(remaining_time)
-            shield["remaining_time"] = remaining_time
-
-        shields = [shields[i] for i in range(len(shields)) if i in valid_shields]
-
-        if len(shields) == 0:
-            tosend += ' none.\nTo register a shield, say "!shield time description" where time is a number between 0 and 72 representing hours. For example,to register an 8 hour shield: "!shield 8 Luggergue"'
-            await message.channel.send(tosend)
-            return
-
-        shields = sorted(shields, key=lambda d: d['remaining_time'], reverse=True)
-
-        for shield in shields:
-            row = f"\n{shield['user']}, {shield['remaining_time']} remaining"
-            if len(shield["description"]) > 0:
-                row = row + f", {shield['description']}"
-
-            tosend = tosend + row
-
-        tosend = tosend + '\n\nRegister a new shield with "!shield time description" where time is a number between 0 and 72 representing hours. For example,to register an 8 hour shield: "!shield 8 Luggergue"'
-
-        with open("shields.pkl", "wb") as fp:
-            pickle.dump(shields, fp)
-
-        await message.channel.send(tosend)
-        return
+        await print_current_shields(message, shields)
 
 keep_alive()
 client.run(os.getenv('TOKEN'))
